501d95a601f6fbf06e40284b09983e6d9bbf8313
miraDask <mira.daskalov@gmail.com>
2020-06-09 19:37:03 +0100

    feat: get all sprints for current project

--- Server/Marathon.Server/Features/Sprints/ISprintService.cs
+++ Server/Marathon.Server/Features/Sprints/ISprintService.cs
@@ -4,8 +4,13 @@
     using System.Collections.Generic;
     using System.Threading.Tasks;
 
+    using Marathon.Server.Features.Common.Models;
+    using Marathon.Server.Features.Sprints.Models;
+
     public interface ISprintService
     {
         Task<int> CreateAsync(int projectId, string title, string goal, int weeks, DateTime startDate);
+
+        Task<ResultModel<IEnumerable<SprintListingServiceModel>>> GetAllByProjecIdAsync(int projectId);
     }
 }
--- Server/Marathon.Server/Features/Sprints/SprintsController.cs
+++ Server/Marathon.Server/Features/Sprints/SprintsController.cs
@@ -6,8 +6,9 @@
     using System.Threading.Tasks;
 
     using Marathon.Server.Features.Common;
+    using Marathon.Server.Features.Common.Models;
     using Marathon.Server.Features.Sprints.Models;
-
+    using Marathon.Server.Infrastructure.Filters;
     using Microsoft.AspNetCore.Mvc;
 
     using static Marathon.Server.Infrastructure.ApiRoutes;
@@ -31,6 +32,7 @@
         /// <response code="401"> Unauthorized request.</response>
         [HttpPost]
         [Route(Sprints.Create)]
+        [TypeFilter(typeof(HasProjectAuthorizationAttribute))]
         public async Task<ActionResult<int>> Create(int projectId, CreateSprintRequestModel input)
         {
             var id = await this.sprintService.CreateAsync(
@@ -41,6 +43,30 @@
                 input.StartDate);
 
             return this.Created(nameof(this.Create), id);
+        }
+
+        /// <summary>
+        /// Get all sprints for current project.
+        /// </summary>
+        /// <response code="200">Returns all sprints for current project.</response>
+        /// <response code="400"> Bad Reaquest.</response>
+        /// <response code="401"> Unauthorized request.</response>
+        [HttpGet]
+        [Route(Sprints.GetAllForProject)]
+        [TypeFilter(typeof(HasProjectAuthorizationAttribute))]
+        public async Task<ActionResult<IEnumerable<SprintListingServiceModel>>> GetAll(int projectId)
+        {
+            var getAllReaquest = await this.sprintService.GetAllByProjecIdAsync(projectId);
+
+            if (!getAllReaquest.Success)
+            {
+                return this.BadRequest(new ErrorsResponseModel
+                {
+                    Errors = getAllReaquest.Errors,
+                });
+            }
+
+            return this.Ok(getAllReaquest.Result);
         }
     }
 }
