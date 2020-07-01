8277143fb795e24c475b099aae14e162a889ea2e
miraDask <mira.daskalov@gmail.com>
2020-07-01 17:21:02 +0100

    feat: add and configure client app

---
+++ Client/marathon-client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Main from './components/main/Main';
+import Header from './components/header/Header';
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<Main />
+		</div>
+	);
+}
+
+export default App;
